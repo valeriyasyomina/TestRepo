9dbe47d4073aed2491a25fb9c4915d6c3102c765
Lera <valeriya.syomina@yandex.ru>
2016-11-09 11:51:00 +0300

    removed

--- src/request.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT(CustomerId) FROM PurchaiseTable P1 
-WHERE P1.ProductName LIKE '%Молоко%' AND NOT EXISTS 
-( 
-	SELECT P2.CustomerId FROM PurchaiseTable P2 
-	WHERE P2.ProductName LIKE '%Сметана%' 
-	AND P2.CustomerId = P1.CustomerId 
-)
